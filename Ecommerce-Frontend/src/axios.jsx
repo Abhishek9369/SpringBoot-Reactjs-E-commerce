36c0cc838f7b495a00b1d4504cdcbf40ffd44277
Abhishek <abhishekk87538@gmail.com>
2026-06-25 03:40:50 +0530

    Update axios.jsx

--- Ecommerce-Frontend/src/axios.jsx
+++ Ecommerce-Frontend/src/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : "http://localhost:8080/api",
+    baseURL: "https://springboot-reactjs-e-commerce.onrender.com/api",
 });
+
 delete API.defaults.headers.common["Authorization"];
 export default API;
